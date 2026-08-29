873f9efda8632486b8ad7fe984dd20c823bfbec5
Ammar Saber <ammarsaber.dev@icloud.com>
2026-08-29 09:46:37 +0300

    Initial Commit

---
+++ Waraq/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Waraq
+//
+//  Created by Ammar Saber on 29/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Waraq/WaraqApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaraqApp.swift
+//  Waraq
+//
+//  Created by Ammar Saber on 29/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WaraqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
